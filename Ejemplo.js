cf9a713275621923d6c6681750d7b28f69db7955
KJBau <bautistake@gmail.com>
2023-07-23 14:38:43 -0500

    tarea2

---
+++ Ejemplo.js
@@ -0,0 +1,13 @@
+function doWhile(num) {
+    // Implementar una función que aumente el valor recibido en 5 hasta un límite de 8 veces.
+    // Retornar el valor final.
+    // Utilizar el bucle Do-While.
+    // Tu código:
+    var x=0;
+    do {
+       x=x+1;
+       num=num+5;
+    } while (x<7);
+ }
+
+console.log(doWhile(1));
